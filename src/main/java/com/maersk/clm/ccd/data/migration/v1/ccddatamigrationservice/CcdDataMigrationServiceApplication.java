a841268287beafb0bc36da684d63bb0a1c08bb10
DEM025 <deepak.emani@maersk.com>
2023-05-10 17:01:58 +0100

    Initial commit

---
+++ src/main/java/com/maersk/clm/ccd/data/migration/v1/ccddatamigrationservice/CcdDataMigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maersk.clm.ccd.data.migration.v1.ccddatamigrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcdDataMigrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcdDataMigrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maersk/clm/ccd/data/migration/v1/ccddatamigrationservice/CcdDataMigrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maersk.clm.ccd.data.migration.v1.ccddatamigrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcdDataMigrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
